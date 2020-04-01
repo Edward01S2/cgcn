bda288b418c5b6f8ab1acd6566752db7a02df578
edward01s2 <edward.01s2@gmail.com>
2020-04-01 15:43:43 -0500

    Fix header make hidden

--- resources/assets/scripts/app.js
+++ resources/assets/scripts/app.js
@@ -14,25 +14,66 @@ $(document).ready(() => {
   })
 
 
-const options = {
-  rootMargin: '0px 0px -80% 0px',
-  threshold: .50 // half of item height
-}
+// const options = {
+//   rootMargin: '0px 0px -80% 0px',
+//   threshold: .50 // half of item height
+// }
 
-const circle = document.querySelector('#sticky_header');
+// const circle = document.querySelector('#sticky_header');
 
-const observer = new IntersectionObserver(entries => {
-  const [{ isIntersecting }] = entries
-  if (isIntersecting) {
-    // console.log('intersected');
-    circle.classList.add('sticky_active');
-  } else {
-    // console.log('not-intersecting');
-    circle.classList.remove('sticky_active');
-  }
-}, options);
+// const observer = new IntersectionObserver(entries => {
+//   const [{ isIntersecting }] = entries
+//   if (isIntersecting) {
+//     // console.log('intersected');
+//     circle.classList.add('sticky_active');
+//   } else {
+//     // console.log('not-intersecting');
+//     circle.classList.remove('sticky_active');
+//   }
+// }, options);
 
-observer.observe(circle);
+// observer.observe(circle);
 
+var header = $("#sticky_header");
+$(window).on("scroll resize", function() {
+  var scroll = $(window).scrollTop();
+  var windowsize = $(window).width();
+
+    if (windowsize <= 612) {
+      //console.log(windowsize)
+      //if the window is greater than 440px wide then turn on jScrollPane..
+      if (scroll >= 225) {
+        header.fadeIn("fast");
+      } else {
+        header.hide();
+      }
+    }
+    if (windowsize <= 768) {
+      console.log(windowsize)
+      //if the window is greater than 440px wide then turn on jScrollPane..
+      if (scroll >= 360) {
+        header.fadeIn("fast");
+      } else {
+        header.hide();
+      }
+    }
+    else {
+      console.log(windowsize)
+      //if the window is greater than 440px wide then turn on jScrollPane..
+      if (scroll >= 475) {
+        header.fadeIn("fast");
+      } else {
+        header.hide();
+      }
+    }
+
+
+
+  // if (scroll >= 450) {
+  //     header.fadeIn("fast");
+  // } else {
+  //     header.hide();
+  // }
+});
 
 });
--- resources/views/blocks/logos.blade.php
+++ resources/views/blocks/logos.blade.php
@@ -1,4 +1,4 @@
-<div id="sticky_header" class="sticky top-0 bg-white shadow shadow-md z-40 transition duration-300">
+<div id="logo-container" class="bg-white shadow shadow-md z-40 transition duration-300">
   <div class="relative">
     <div class="text-center absolute top-0 left-0 right-0 -mt-6">
       <div class="sticky-subtitle bg-c-blue-200 text-white font-semibold px-8 tracking-wider py-2 inline-block text-sm md:text-base lg:text-lg transition duration-300">{!! $subtitle!!}</div>
@@ -12,6 +12,22 @@
         <span class="font-caslon font-black text-4xl px-8 md:text-6xl lg:px-12">&</span>
         <a class="block hover:opacity-75 transition duration-300 transform" href="{!! $logos[1]['Logo Link']['url'] !!}" target="_blank">
           <img class="h-12 md:h-20 lg:h-24 transition duration-300" src="{!! $logos[1]['Logo']['url'] !!}" alt="">
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="sticky_header" class="hidden sticky top-0 bg-white shadow shadow-md z-40 transition duration-300">
+  <div class="relative">
+    <div class="sticky-container flex items-center justify-center py-2 pt-3">
+      <div class="sticky-text mr-4 pb-1 font-semibold md:text-lg md:mr-8">{!! $sticky !!}</div>
+      <div class="sticky-logos flex items-center justify-center">
+        <a class="block hover:opacity-75 transition duration-300" href="{!! $logos[0]['Logo Link']['url'] !!}" target="_blank">
+          <img class="h-8 transition duration-300" src="{!! $logos[0]['Logo']['url'] !!}" alt="">
+        </a>
+        <span class="font-caslon font-black text-xl px-2 pb-1">&</span>
+        <a class="block hover:opacity-75 transition duration-300 transform" href="{!! $logos[1]['Logo Link']['url'] !!}" target="_blank">
+          <img class="h-8 transition duration-300" src="{!! $logos[1]['Logo']['url'] !!}" alt="">
         </a>
       </div>
     </div>
